acf0aaae10792c741c00d1bfcbe6042204888e9c
Urtu240z <73882741+Urtu240z@users.noreply.github.com>
2026-08-22 11:27:38 +0200

    more fixes

--- lab/coastal/empty_proj/main.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-func _ready():
-	var f := FileAccess.open("C:/Users/ehort/Documents/GODOT PROJECTS/Water Race Ocean Lab/lab/coastal/empty_proj_ran.txt", FileAccess.WRITE)
-	if f != null:
-		f.store_line("EMPTY_PROJ_READY")
-		f.close()
-	get_tree().quit(0)
-
